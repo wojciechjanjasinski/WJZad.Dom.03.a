d1b6bbf268740d33b221557ca468dd4eb4c68cd6
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-10 14:23:35 +0200

    Add constructor in Trackball.java and use it in TrackballShop.java

--- src/Trackball.java
+++ src/Trackball.java
@@ -9,4 +9,17 @@ class Trackball {
     int buttonNumber;
     boolean scrollWhell;
     boolean inProduction;
+
+    Trackball(String n, String p, String t, String pH, int prY, int bS, boolean w, int bN, boolean sW, boolean iP) {
+        name = n;
+        producer = p;
+        preferedHand = pH;
+        ballSize = 0;
+        wireless = false;
+        buttonNumber = 3;
+        scrollWhell = true;
+        inProduction = true;
+
+    }
+
 }
--- src/TrackballShop.java
+++ src/TrackballShop.java
@@ -1,29 +1,7 @@
 class TrackballShop {
     public static void main(String[] args) {
-        Trackball trackball1 = new Trackball();
-
-        trackball1.name = "Trackball Explorer";
-        trackball1.producer = "Microsoft";
-        trackball1.type = "finger-operated";
-        trackball1.preferedHand = "right hand";
-        trackball1.productionYear = 2001;
-        trackball1.ballSize = 38;
-        trackball1.wireless = false;
-        trackball1.buttonNumber = 5;
-        trackball1.scrollWhell = true;
-        trackball1.inProduction = false;
-
-        Trackball trackball2 = new Trackball();
-        trackball2.name = "Trackman Marble";
-        trackball2.producer = "Logitech";
-        trackball2.type = "finger-operated";
-        trackball2.preferedHand = "ambidexious";
-        trackball2.productionYear = 2004;
-        trackball2.ballSize = 40;
-        trackball2.wireless = false;
-        trackball2.buttonNumber = 4;
-        trackball2.scrollWhell = false;
-        trackball2.inProduction = true;
+        Trackball trackball1 = new Trackball("Trackball Explorer", "Microsoft", "finger-operated", "right hand", 2001, 38, false, 5, true, false);
+        Trackball trackball2 = new Trackball("Trackman Marble", "Logitech", "finger-operated", "ambidexious", 2004, 40, false, 4, false, true);
 
         System.out.println(trackball1.producer + " " + trackball1.name + " " + trackball1.type + " " + trackball1.preferedHand + " " + trackball1.productionYear + " " + trackball1.ballSize + " mm ball " + "wireless: " + trackball1.wireless + " " + trackball1.buttonNumber + " buttons " + "Scroll Wheel: " +  trackball1.scrollWhell + " still in production: " + trackball1.inProduction);
         System.out.println(trackball2.producer + " " + trackball2.name + " " + trackball2.type + " " + trackball2.preferedHand + " " + trackball2.productionYear + " " + trackball2.ballSize + " mm ball " + "wireless: " + trackball2.wireless + " " + trackball2.buttonNumber + " buttons " + "Scroll Wheel: " +  trackball2.scrollWhell + " still in production: " + trackball2.inProduction);
