43a55d6d28205e01af76b8a101e39db33f7950ee
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-10 14:04:32 +0200

    Create Trackball.java and TrackballShop.java files with trackball models description.

---
+++ src/Trackball.java
@@ -0,0 +1,12 @@
+class Trackball {
+    String name;
+    String producer;
+    String type;
+    String preferedHand;
+    int productionYear;
+    int ballSize;
+    boolean wireless; // true = bezprzewodowy false = kablowy
+    int buttonNumber;
+    boolean scrollWhell;
+    boolean inProduction;
+}
