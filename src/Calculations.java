418f3c42129b9355068a05d1779b7900f464d3da
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-12 20:51:20 +0200

    Getting better code.

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,23 +1,16 @@
-import javax.swing.*;
-
-import static java.lang.Math.PI;
-import static java.lang.Math.pow;
-
 class Calculations {
     public static void main(String[] args) {
         MathOperations math1 = new MathOperations();
-        math1.x = 7;
+
+
         boolean isEven = math1.isEven(2001);
         System.out.println(isEven);
         boolean isOdd = math1.isOdd(2001);
         System.out.println(isOdd);
-
-        double d = Math.PI * math1.x + pow(math1.x, 2);
-        System.out.println(d);
-
-        double e = pow(math1.x, 2);
-        System.out.println(e);
-
+        double circleField = math1.circleField(5);
+        System.out.println(circleField);
+        double power = math1.power(7);
+        System.out.println(power);
     }
 }
 
--- src/MathOperations.java
+++ src/MathOperations.java
@@ -1,10 +1,20 @@
+import static java.lang.Math.pow;
+
 class MathOperations {
 
-    int x;
     boolean isEven(int x) {
         return x % 2 == 0;
     }
+
     boolean isOdd(int x) {
         return x % 2 == 1;
+    }
+
+    double circleField(double a) {
+        return Math.PI * a + pow(a, 2);
+    }
+
+    double power(double a) {
+        return pow(a, 2);
     }
 }
