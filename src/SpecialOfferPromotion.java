f52c71b56666fec947cd27fd25fafede1eb5e3a3
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-14 13:29:34 +0200

    Small changes.

--- src/SpecialOfferPromotion.java
+++ src/SpecialOfferPromotion.java
@@ -1,3 +1,3 @@
 class SpecialOfferPromotion {
-    Product specialOfferPromotion = new Product("Krówki", "Wawel2", 6.99, "Łakoć doskonała od jednego z najbarrdzej cenionych producentów w Polsce", true, 2.01, new Category("Słodkie", "Łakocie dla wszystkich"));
+    Product specialOfferPromotion = new Product("Krówki", "Wawel2", 6.99, "Łakoć doskonała od jednego z najbarrdzej cenionych producentów w Polsce", true, 5.59, new Category("Słodkie", "Łakocie dla wszystkich"));
 }
